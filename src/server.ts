1c15638903e0e2b69daf330408a9d423b9cd82d6
Rafael Dias <rafaeldias.consultor@icloud.com>
2019-06-21 18:27:28 -0300

    Fix heroku deploy

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { app } from './app';
 import * as http from 'http';
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 const server = http.createServer(app);
 
 server.listen(port);
